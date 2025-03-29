443d89ace66f5e4e418cc6ca52ff472e7f318fa4
Eric Ge <eric.ge96@gmail.com>
2025-03-29 23:02:26 +0000

    wrong amazon scraper

--- backend/scraper/amazon.py
+++ backend/scraper/amazon.py
@@ -1,7 +1,5 @@
 import requests
 import os
-import json
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,15 +8,13 @@ load_dotenv()
 def scrape_amazon(keyword):
     payload = { 'api_key': SCRAPER_API_KEY, 'url': f'https://www.amazon.com/s?k={keyword.replace(' ', '+')}', 'output_format': 'json', 'autoparse': 'true' }
     r = requests.get('https://api.scraperapi.com/', params=payload)
-    return r.text
+    return r.json()["results"]
     
 
 def parse_amazon_data(json_data):
-    products = json.loads(json_data)
     
     parsed_products = []
-    products = products.get("results", [])[:20]
-    print("Parsed JSON:", json.dumps(products, indent=2))
+    products = json_data[:20]
     
     for product in products:
         parsed_products.append({

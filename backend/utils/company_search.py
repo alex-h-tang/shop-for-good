27c6878e253a0aa8b65a527ee2f41d8bceb3b058
Eric Ge <eric.ge96@gmail.com>
2025-03-29 23:09:17 +0000

    api key leak

--- backend/utils/company_search.py
+++ backend/utils/company_search.py
@@ -20,14 +20,16 @@ def fuzzy_search(input, data):
     names = [company['companyname'] for company in data]
     return process.extractOne(input, names)
 
-# api call to get esg json for company
-url = "https://gaialens-esg-scores.p.rapidapi.com/scores"
-headers = {
-	"x-rapidapi-key": "5a768ad3fcmsh267709c9db7f529p195befjsn0c88469fb16e",
-	"x-rapidapi-host": "gaialens-esg-scores.p.rapidapi.com",
-	"Content-Type": "text"
-}
+
 def get_esg(company):
+    # api call to get esg json for company
+    url = "https://gaialens-esg-scores.p.rapidapi.com/scores"
+    api_key = os.getenv("X_RAPIDAPI_KEY")
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": "gaialens-esg-scores.p.rapidapi.com",
+        "Content-Type": "text"
+    }
     # encoded_company = urllib.parse.quote(company)
     querystring = {"companyname":company}
     print(querystring)
